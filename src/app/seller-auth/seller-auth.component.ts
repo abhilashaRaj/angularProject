34f355cbb3559cc6aec2a8204ad0e8861ad65157
abhilashaRaj <abhilasharaj83@gmail.com>
2024-05-20 22:24:47 +0530

    First commit

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent  {
+constructor(private seller: SellerService) {}
+  signUp(data:object):void{
+    console.warn(data)
+  }
+}
